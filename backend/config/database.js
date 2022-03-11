ec5a1bef925a71949f3ad85911934e5ffb61eadf
Hendson <hendsonszeto@live.com>
2022-03-10 17:33:47 -0800

    change successful database connection message

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDatabase = async () => {
     try {
         mongoose.connect(process.env.DB_STRING, {useNewUrlParser: true, useUnifiedTopology: true});        
-        console.log('Successfully connected!');
+        console.log('Successfully connected to MongoDB!');
     } catch (error) {
         console.log(error);
         process.exit(1);
